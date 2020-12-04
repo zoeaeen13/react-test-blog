4c3241629786780f0465edaa7e9ce97d0559b373
Menghua <zoeaeen13@gmail.com>
2020-12-04 16:45:50 +0800

    update package

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import HomePage from "../pages/Home";
+import AboutPage from "../pages/About";
+import LoginPage from "../pages/Login";
+import RegisterPage from "../pages/Register";
+import NewPostPage from "../pages/NewPost";
+import PostPage from "../pages/Post";
+import ArchivePage from "../pages/Archive";
+import { AuthContext } from "../contexts";
+import { getUserInfo } from "../WebAPI";
+import { getAuthToken, setAuthToken } from "../utills";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // init login status
+    if (getAuthToken()) {
+      getUserInfo().then((response) => {
+        if (!response.ok) {
+          return setAuthToken(null);
+        }
+        setUser(response.data);
+      });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <div>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/register">
+              <RegisterPage />
+            </Route>
+            <Route path="/post">
+              <NewPostPage />
+            </Route>
+            <Route path="/archive">
+              <ArchivePage />
+            </Route>
+            <Route path="/posts/:slug">
+              <PostPage />
+            </Route>
+            <Route path="/user/:slug">
+              <ArchivePage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/pages/Archive/ArchivePage.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { getAllPosts, getUserPosts } from "../../WebAPI";
+import { useParams } from "react-router-dom";
+import Intro from "../../components/Intro";
+import Pagination from "../../components/Pagination";
+import {
+  HomePageRoot,
+  HomeWrapper,ArchiveItem,ArchiveTitle,
+} from "../../components/Home";
+
+function ArchivePage() {
+  let { slug } = useParams();
+  const [userPosts, setUserPosts] = useState([]);
+  const [author, setAuthor] = useState(null);
+  const [archivePosts, setArchivePosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  const [recentPosts, setRecentPosts] = useState([]);
+  const handleChangePage = (page) => {
+    setCurrentPage(page);
+  };
+
+  // init
+  useEffect(() => {
+    // specific user posts
+    if (slug) {
+      getUserPosts(slug).then((response) => {
+        if (response.posts.length > 0) {
+          setUserPosts(response.posts.reverse());
+        }
+        setAuthor({
+          nickname: response.nickname,
+          postNum: response.posts.length,
+        });
+      });
+    } else {
+      // archive
+      getAllPosts().then((posts) => {
+        if (posts.length > 0) {
+          let quotient = Math.ceil(posts.length / 5);
+          if (!posts.length % 5) {
+            quotient += 1;
+          }
+          setArchivePosts(posts);
+          setCurrentPage(1);
+          setTotalPage(quotient);
+        }
+      });
+    }
+  }, [slug]);
+
+  // change pages
+  useEffect(() => {
+    const index = (currentPage - 1) * 5;
+    setRecentPosts(archivePosts.slice(index, index + 5));
+  }, [archivePosts, currentPage]);
+
+  return (
+    <HomePageRoot>
+      <Intro />
+      <HomeWrapper>
+        {!slug && <ArchiveTitle>所有文章</ArchiveTitle>}
+        {slug && author && (
+          <ArchiveTitle>
+            '{author.nickname}' 目前發表了 {author.postNum} 篇文章
+          </ArchiveTitle>
+        )}
+        {recentPosts &&
+          recentPosts.map((post) => <ArchiveItem key={post.id} post={post} />)}
+        {slug &&
+          userPosts.map((post) => <ArchiveItem key={post.id} post={post} />)}
+        {!slug && recentPosts && totalPage && (
+          <Pagination
+            current={currentPage}
+            total={totalPage}
+            handleChangePage={handleChangePage}
+          />
+        )}
+      </HomeWrapper>
+    </HomePageRoot>
+  );
+}
+
+export default ArchivePage;
